1fa0a017a6b8b88ebc1b77ddfa62ebc2cfc7f852
AnastasiiaLAV <95190359+AnastasiiaLAV@users.noreply.github.com>
2022-08-13 23:43:57 +0000

    Deploying to gh-pages from @ AnastasiiaLAV/goit-react-hw-03-image-finder@c93872f7cd734fdaf64b8b869639c0e833002df9 🚀

---
+++ precache-manifest.7a26b3d41a6df13816a83de52cda9967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26c1e8d02471052e6598",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.419ff971.chunk.css"
+  },
+  {
+    "revision": "26c1e8d02471052e6598",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.fe8b76c4.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  },
+  {
+    "revision": "e0ff865c0e4ac34512d5",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.7137d3a1.chunk.js"
+  },
+  {
+    "revision": "4776f02c7860380a0707d78e066f4f0e",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  }
+];
